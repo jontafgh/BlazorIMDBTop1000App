dcf99bf7da07e5bb4780c9adc9cf346772da4a0f
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-11 11:13:51 +0200

    Add project files.

---
+++ BlazorIMDBTop1000App/Movie.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorIMDBTop1000App
+{
+    public class Movie
+    {
+        public string? PosterLink { get; set; }
+        public string? SeriesTitle { get; set; }
+        public string? ReleasedYear { get; set; }
+        public string? Certificate { get; set; }
+        public string? Runtime { get; set; }
+        public string? Genre { get; set; }
+        public string? IMDBRating { get; set; }
+        public string? Overview { get; set; }
+        public string? Metascore { get; set; }
+        public string? Director { get; set; }
+        public string? Star1 { get; set; }
+        public string? Star2 { get; set; }
+        public string? Star3 { get; set; }
+        public string? Star4 { get; set; }
+        public string? NoofVotes { get; set; }
+        public string? Gross { get; set; }
+    }
+}
---
+++ BlazorIMDBTop1000App/MovieCsvDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace BlazorIMDBTop1000App
+{
+    public class MovieCsvDataAccess
+    {
+        public List<Movie> GetImdbTop1000Movies()
+        {
+            CsvUtilities<Movie> csvUtilities = new CsvUtilities<Movie>();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                PrepareHeaderForMatch = header => Regex.Replace(header.Header, @"_", string.Empty)
+                
+            };
+
+            return csvUtilities.ReadCsv("Data/imdb_top_1000.csv", config).ToList();
+        }
+    }
+}
