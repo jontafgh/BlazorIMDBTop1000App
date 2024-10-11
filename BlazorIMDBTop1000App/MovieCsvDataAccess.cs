a20c8e3f0c025cb119fcadb9a0b15958a3ea270f
jonatan.thunell@yahoo.com <jonatan.thunell@yahoo.com>
2024-10-12 00:42:54 +0200

    can show movies

--- BlazorIMDBTop1000App/MovieCsvDataAccess.cs
+++ BlazorIMDBTop1000App/MovieCsvDataAccess.cs
@@ -6,7 +6,7 @@ namespace BlazorIMDBTop1000App
 {
     public class MovieCsvDataAccess
     {
-        public List<Movie> GetImdbTop1000Movies()
+        private static List<Movie> GetImdbTop1000()
         {
             using (var reader = new StreamReader("Data/imdb_top_1000.csv"))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -23,6 +23,16 @@ namespace BlazorIMDBTop1000App
                 }
                 return records;
             }
+        }
+        public Dictionary<Movie, bool> GetImdbTop1000Movies()
+        {
+            Dictionary<Movie, bool> imdbTop1000 = new Dictionary<Movie, bool>();
+
+            foreach (var movie in GetImdbTop1000())
+            {
+                imdbTop1000.Add(movie, false);
+            }
+            return imdbTop1000;
         }
     }
 
